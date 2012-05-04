d97a82377ecc40bd03e4a36bd29f87be0d2546b5
Anthony Brummett <brummett@gmail.com>
2012-05-04 18:28:33 -0500

    Add prerequsite libs for jquery, bootstrap and sammy.js

--- manager/vendor/couchapp/_attachments/loader.js
+++ manager/vendor/couchapp/_attachments/loader.js
@@ -8,10 +8,12 @@ function couchapp_load(scripts) {
 couchapp_load([
   "/_utils/script/sha1.js",
   "/_utils/script/json2.js",
-  "/_utils/script/jquery.js",
+   "vendor/jquery/jquery-1.7.2.js",
   "/_utils/script/jquery.couch.js",
   "vendor/couchapp/jquery.couch.app.js",
   "vendor/couchapp/jquery.couch.app.util.js",
   "vendor/couchapp/jquery.mustache.js",
-  "vendor/couchapp/jquery.evently.js"
+  "vendor/couchapp/jquery.evently.js",
+  "vendor/sammy.js/sammy.js"
+  "vendor/twitter/bootstrap.js"
 ]);

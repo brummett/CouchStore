c47c439a5878af87480711099c99eb5f72648d39
Anthony Brummett <brummett@gmail.com>
2012-05-15 23:19:36 -0500

    list-items activity working a bit better
    edit link/button is working
    
    search is now done with get()

--- manager/_attachments/javascripts/activity.js
+++ manager/_attachments/javascripts/activity.js
@@ -199,51 +199,113 @@ $.couch.app(function(couchapp) {
 
         this.get('#/list-items', function(context) {
             context.log("In list-items");
-            context.render('templates/activity-listItems.template')
-                    .swap( function() {
-                                // After creating the list-items activity, trigger a search
-                                // on an empty string which will populate the initial list
-                                context.$element('form#search').trigger('submit');
-                        });
-        });
 
-        this.post('#/search/:type', function(context) {
-            var tableContainer = $('#search-results');
-            var searchTerm = $('#search-query').val();
-
-            var filter, viewName, showName;
-            if (context.params['type'] == 'item') {
-                viewName = 'items-by-name',
-                filter = searchTerm ? function(doc) { 
+            var searchTerm = context.params['search-query'];
+            var filter = searchTerm ? function(doc) {
                                             return ((doc.name && (doc.name.toString().toLowerCase().indexOf(searchTerm) > -1 ))
                                                 || (doc.sku && (doc.sku.toString().toLowerCase().indexOf(searchTerm) > -1 ))
                                                 || (doc.barcode && (doc.barcode.toString().toLowerCase().indexOf(searchTerm) > -1 ))
                                                 || (doc.desc && (doc.desc.toString().toLowerCase().indexOf(searchTerm) > -1 ))
                                             ); }
                                     : function (doc) { return 1; };
-                showName = '_show/listItemRow/';
-            }
-
-            couchapp.view(viewName, {
+            couchapp.view('items-by-name', {
                 include_docs: true,
                 success: function(data) {
-                    tableContainer.empty();
+                    var items = [];
                     for (var i in data.rows) {
                         var doc = data.rows[i].doc;
                         if (filter(doc)) {
-                            context.load(showName + doc['_id'])
-                                    .then(function(content) {
-                                            tableContainer.append(content);
-                                        });
+                            items.push(doc);
                         }
                     }
+                    searchTerm = searchTerm || '';
+                    context.render('templates/activity-listItems.template', { items: items, searchTerm: searchTerm })
+                            .swap();
                 }
             });
-
-        this.get('#/edit/:type/:id', function(context) {
-
         });
-        this.post('#/edit/:thing/:id', function(context) {
+
+        this.get('#/edit/item/:id', function(context) {
+            var show_q = '_show/editItem';
+            if (context.params['id']) {
+                show_q += '/' + context.params['id'];
+            }
+            context.load(show_q)
+                    .then(function(content) {
+                            var modal = $(content).appendTo(context.$element())
+                                                    .modal({backdrop: true, keyboard: true, show: true});
+                            modal.on('hidden', function() { modal.remove(); window.history.back() });
+                        });
+        });
+        this.post('#/edit/item/:id', function(context) {
+            var validationError = 0,
+                this_id = context.params['id'],
+                modal = $('.modal');
+
+            var markError = function(field, message) {
+                $('fieldset#' + field + '-field').addClass('error');
+                $('input#' + field).after('<span class="help-inline"><i class="icon-exclamation-sign"></i>&nbsp;'
+                                            + message + '</span>');
+            };
+            var checkFieldHasValue = function(field) {
+                var value = context.params[field];
+                if ((value == undefined) || (value == '')) {
+                    markError(field, 'Required');
+                    validationError = 1;
+                }
+            };
+            var checkDuplicateField = function(field, callback) {
+                var value = context.params[field];
+                couchapp.view('items-by-'+field, {
+                    key: value,
+                    success: function(data) {
+                        if (data.rows.length && data.rows[0].id != this_id) {
+                            // Found an item with the form's sku/barcode and it's a different doc id
+                            markError(field, 'Duplicate');
+                            validationError = 1;
+                        }
+                        callback();
+                    }
+                });
+            };
+            var saveItem = function(context) {
+                if (validationError) {
+                    $.log('not saving because of errors');
+                    return false;
+                }
+                var doc = { type: 'item' };
+                if (context.params['_id']) {
+                    doc['_id'] = context.params['_id'];
+                    doc['_rev'] = context.params['_rev'];
+                }
+                doc['barcode']      = context.params['barcode'];
+                doc['name']         = context.params['name'];
+                doc['sku']          = context.params['sku'];
+                doc['description']  = context.params['description'];
+                doc['cost-cents']   = context.params['cost'] * 100;
+                doc['price-cents']  = context.params['price'] * 100;
+                $.log(doc);
+
+                couchapp.db.saveDoc(doc, {
+                    success: function(data) {
+                        modal.modal('hide');
+                        showNotification('success', 'Item saved');
+                    },
+                    error: function(status) {
+                        showNotification('error', 'Problem saving');
+                    }
+                });
+            };
+
+            checkFieldHasValue('sku');
+            checkFieldHasValue('barcode');
+            checkFieldHasValue('name');
+
+            checkDuplicateField('barcode',
+                function() { checkDuplicateField('sku',
+                    function() { saveItem(context) }
+                )}
+            );
 
         });
 
---
+++ manager/shows/editItem.js
@@ -0,0 +1,21 @@
+function(doc, req) {
+    var ddoc = this;
+    var Mustache = require('vendor/couchapp/lib/mustache');
+
+    var data = {};
+    if(doc) {
+        data._id = doc._id;
+        data._rev = doc._rev;
+        data.name = doc.name;
+        data.sku = doc.sku;
+        data.barcode = doc.barcode;
+        data.cost = doc['cost-cents'] ? (parseFloat(doc['cost-cents']) / 100).toFixed(2) : "0.00";
+        data.price = doc['price-cents'] ? (parseFloat(doc['price-cents']) / 100).toFixed(2) : "0.00";
+        data.description = doc.description;
+        data['add-edit-title'] = "Edit";
+    } else {
+        data['add-edit-title'] = "Add";
+    }
+
+    return Mustache.to_html(ddoc.templates['editItem'], data);
+}

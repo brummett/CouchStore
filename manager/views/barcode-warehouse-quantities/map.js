d81a1fe609d25e0f1fddbe9f4bba1d75ee1232f4
Anthony Brummett <brummett@gmail.com>
2012-09-13 19:12:12 -0500

    Change the counting for inventory items
    items listed in the unfilled-items list are counted against the inventory

--- manager/views/barcode-warehouse-quantities/map.js
+++ manager/views/barcode-warehouse-quantities/map.js
@@ -1,10 +1,26 @@
 function(doc) {
     if (doc.type == 'order') {
-        if (doc.items) {
-            var barcode = '',
-                warehouse_id = doc['warehouse-id'] || '';
-            for (barcode in doc.items) {
-                emit([barcode, warehouse_id], doc.items[barcode]);
+        var barcode = '',
+            warehouse_id = doc['warehouse-id'] || '<unknown>',
+            already_printed = {},
+            count,
+            barcode;
+
+        if ('items' in doc) {
+            for (barcode in doc['items']) {
+                count = doc['items'][barcode];
+                if ('unfilled-items' in doc) {
+                    count += doc['unfilled-items'][barcode];
+                }
+                already_printed[barcode] = true;
+                emit([barcode, warehouse_id], count);
+            }
+        }
+        if ('unfilled-items' in doc) {
+            for (barcode in doc['unfilled-items']) {
+                if (already_printed[barcode]) continue;
+
+                emit([barcode, warehouse_id], doc['unfilled-items'][barcode]);
             }
         }
     }
--- manager/views/warehouse-barcode-quantities/map.js
+++ manager/views/warehouse-barcode-quantities/map.js
@@ -1,10 +1,26 @@
 function(doc) {
     if (doc.type == 'order') {
-        if (doc.items) {
-            var barcode = '',
-                warehouse_id = doc['warehouse-id'] || '';
-            for (barcode in doc.items) {
-                emit([warehouse_id, barcode], doc.items[barcode]);
+        var barcode = '',
+            warehouse_id = doc['warehouse-id'] || '<unknown>',
+            already_printed = {},
+            count,
+            barcode;
+
+        if ('items' in doc) {
+            for (barcode in doc['items']) {
+                count = doc['items'][barcode];
+                if ('unfilled-items' in doc) {
+                    count += doc['unfilled-items'][barcode];
+                }
+                already_printed[barcode] = true;
+                emit([warehouse_id, barcode], count);
+            }
+        }
+        if ('unfilled-items' in doc) {
+            for (barcode in doc['unfilled-items']) {
+                if (already_printed[barcode]) continue;
+
+                emit([warehouse_id, barcode], doc['unfilled-items'][barcode]);
             }
         }
     }

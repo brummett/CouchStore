5e0f9dbebde1152a08bd02f68bee916ff1b6f1ff
Anthony Brummett <brummett@gmail.com>
2012-09-14 11:30:24 -0500

    Perform the filtering on the key instead of the values

--- manager/lists/items.js
+++ manager/lists/items.js
@@ -8,13 +8,8 @@ function(head,req) {
 
     itemType = itemType.substr(0, itemType.indexOf('-')); // get the type up to the first '-'
     var matches = search
-                ? function(doc) {
-                    return ((doc.name && (doc.name.toString().toLowerCase().indexOf(search) > -1 ))
-                        || (doc.sku && (doc.sku.toString().toLowerCase().indexOf(search) > -1 ))
-                        || (doc.barcode && (doc.barcode.toString().toLowerCase().indexOf(search) > -1 ))
-                        || (doc.desc && (doc.desc.toString().toLowerCase().indexOf(search) > -1 ))
-                        ); }
-                : function(doc) { return 1; };
+                ? function(key) { return key.toString.toLowerCase.indexOf(search) > -1; }
+                : function(key) { return 1; };
                     
 
     provides('html', function() {
@@ -24,7 +19,7 @@ function(head,req) {
                 path: '#/data/' + itemType + '/',
             };
         while (row = getRow()) {
-            if (! shown[row.id] && matches(row.value)) {
+            if (! shown[row.id] && matches(row.key)) {
                 shown[row.id] = true;  // Don't show the same item more than once
                 row.value._id = row.id;
                 data.items.push(row.value);

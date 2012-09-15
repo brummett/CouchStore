a7d1bda0857407ca63229c9db1e507079a8f0095
Anthony Brummett <brummett@gmail.com>
2012-09-14 22:43:39 -0500

    Editing a sale order works

--- manager/shows/edit-order.js
+++ manager/shows/edit-order.js
@@ -3,7 +3,10 @@ function(doc, req) {
         Mustache = require('vendor/couchapp/lib/mustache'),
         data = {},
         templateName = '',
-        i = 0;
+        i = 0,
+        itemKey = '',
+        shipServiceLevels = [ 'standard', 'expedited', 'overnight'],
+        orderSources = [ 'web', 'amazon','phone','ebay','buy.com'];
 
     if (doc) {
         if ( doc.type != 'order' ) {
@@ -15,6 +18,7 @@ function(doc, req) {
 
         templateName = doc['order-type'] + '-order';
 
+        data.action = '#/order/' + doc['order-type'] + '/';
         data.title = 'Edit ' +  doc['order-type'] + ' order';
         data.date = doc.date;
         data.orderNumber = doc._id.substr(6);  // order docs start with the text 'order-'
@@ -23,9 +27,17 @@ function(doc, req) {
         data.warehouseName = doc['warehouse-name'];
         data.warehouseId = doc['warehouse-id'];
         data._rev = doc._rev;
+
+        if (doc['order-type'] == 'receive') {
+            itemKey = 'items';
+        } else if (doc['order-type'] == 'sale') {
+            itemKey = 'unfilled-items';
+        } else {
+            itemKey = 'items';
+        }
         data.items = [];
-        for (i in doc.items) {
-            data.items.push({ barcode: i, quantity: doc.items[i], cost: ((doc['item-costs'][i]/100).toFixed(2)) });
+        for (i in doc[itemKey]) {
+            data.items.push({ barcode: i, quantity: Math.abs(doc[itemKey][i]), cost: ((doc['item-costs'][i]/100).toFixed(2)) });
         }
         
     } else {
@@ -38,6 +50,7 @@ function(doc, req) {
 
         templateName = req.query.type + '-order';
 
+        data.action = '#/order/' + req.query.type + '/';
         data.title = 'New ' +req.query.type + ' order';
         data.date = '';
         data.orderNumber = '';
@@ -49,8 +62,9 @@ function(doc, req) {
         data.costs = [];
     }
 
-    data.action = '#/order/' + req.query.type + '/';
     data.allowDelete = true;
+    data.shipServiceLevels = shipServiceLevels;
+    data.orderSources = orderSources;
 
     return Mustache.to_html(ddoc.templates[templateName], data, ddoc.templates.partials['edit-order']);
 }

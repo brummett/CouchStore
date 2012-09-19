d040d2155c19247ed8f0dbb4f8b197439a5a91bb
Anthony Brummett <brummett@gmail.com>
2012-09-18 22:42:49 -0500

    in the picklist-order-picker, designate orders with backerdered items
    prepend the order number with "Backordered:"
    
    I'd rather use a different color or something, but I guess you can't style
    option elements

--- manager/lists/picklist-order-picker.js
+++ manager/lists/picklist-order-picker.js
@@ -7,10 +7,12 @@ function(head,req) {
         row;
 
     provides('html', function() {
+        var typeaheadData = [];
         while( row = getRow() ) {
-            data.orders.push({ id: row.id, value: row.value});
+            data.orders.push({ id: row.id, isBackordered: row.value.isBackordered, message: row.value.message});
+            typeaheadData.push(row.value.message);
         }
-        data.typeaheadSource = JSON.stringify(data.orders);
+        data.typeaheadSource = JSON.stringify(typeaheadData);
 
         return Mustache.to_html(ddoc.templates['picklist-order-picker'], data);
     });
--- manager/views/picklist-order-picker/map.js
+++ manager/views/picklist-order-picker/map.js
@@ -3,6 +3,7 @@ function(doc) {
         // order doc IDs start with the string 'order-'
         var order_number,
             barcode,
+            isBackordered = doc.shipments ? doc.shipments.length : 0,
             shipping = { standard: 10, expedited: 5, overnight: 1 },
             count = 0;
         
@@ -14,7 +15,9 @@ function(doc) {
         if (count > 0) {
             order_number = doc._id.substring(6);
             emit([order_number, shipping[doc['shipping-service-level']]],
-                order_number + ' ' + count + ' items from '+ doc['warehouse-name'] + ' ' + doc['shipping-service-level'] + ' shipping');
+                { message: order_number + ' ' + count + ' items from '+ doc['warehouse-name'] + ' ' + doc['shipping-service-level'] + ' shipping',
+                  isBackordered: isBackordered
+                });
         }
     }
 }

6568cd83d6d82e32a392b21760b6859817df3958
Anthony Brummett <brummett@gmail.com>
2012-09-21 18:37:17 -0500

    data lister "edit" bug fix
    Previously, when dismissing the edit modal, it wasn't moving the URL history
    back, leaving it like "/edit/item/UUID

--- manager/_attachments/javascripts/activity.js
+++ manager/_attachments/javascripts/activity.js
@@ -178,7 +178,7 @@ $.couch.app(function(couchapp) {
                                     d.resolve(modal);
                                 });
                     });
-                return d.promise;
+                return d.promise();
             },
 
             errorModal: function(title, message) {

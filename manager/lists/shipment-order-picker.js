ebc4f319d49ed8264f7968a758536392b1de6515
Anthony Brummett <brummett@gmail.com>
2012-10-04 23:49:26 -0500

    Get rid of the empty first item in the Make a Shipment dropdown box

--- manager/lists/shipment-order-picker.js
+++ manager/lists/shipment-order-picker.js
@@ -3,12 +3,13 @@
 function(head,req) {
     var ddoc = this,
         Mustache = require('vendor/couchapp/lib/mustache'),
-        data = { orders: [], typeaheadSource: ''},
+        data = { orders: [], typeaheadSource: '' },
         row;
 
     provides('html', function() {
         var typeaheadData = [];
         while( row = getRow() ) {
+            data.firstOrderNumber = data.firstOrderNumber || row.id.substr(6);
             data.orders.push({ id: row.id, isBackordered: row.value.isBackordered, message: row.value.message});
             typeaheadData.push(row.value.message);
         }

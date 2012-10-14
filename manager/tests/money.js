4faaae5d029dc45542fd2db64f7c4387cff7a505
Anthony Brummett <brummett@gmail.com>
2012-10-14 13:59:54 -0500

    Test the money module
    found a bug in the dollars and cents regex

---
+++ manager/tests/money.js
@@ -0,0 +1,37 @@
+// Test the money module
+var money = require(__dirname + '/../views/lib/money');
+var assert = require('assert');
+
+describe('Money module', function() {
+    it('converts to cents', function() {
+        assert.equal(money.toCents(1.23), 123);
+        assert.equal(money.toCents(123), 12300);
+        assert.equal(money.toCents(0.12), 12);
+        assert.equal(money.toCents(0), 0);
+    });
+
+    it('converts to dollars', function() {
+        assert.equal(money.toDollars(123), 1.23);
+        assert.equal(money.toDollars(12), 0.12);
+        assert.equal(money.toDollars(0), 0);
+    });
+
+    it('matches dollars', function() {
+        assert.ok(money.isDollarsAndCents('1.23'));
+        assert.ok(money.isDollarsAndCents('1'));
+        assert.ok(money.isDollarsAndCents('123'));
+        assert.ok(money.isDollarsAndCents('0.23'));
+        assert.ok(money.isDollarsAndCents('.23'));
+
+        assert.ok(! money.isDollarsAndCents(''));
+        assert.ok(! money.isDollarsAndCents('1.'));
+        assert.ok(! money.isDollarsAndCents('1.234'));
+        assert.ok(! money.isDollarsAndCents('a'));
+        assert.ok(! money.isDollarsAndCents(' '));
+        assert.ok(! money.isDollarsAndCents('.'));
+        assert.ok(! money.isDollarsAndCents('.2'));
+        assert.ok(! money.isDollarsAndCents('1.2'));
+        assert.ok(! money.isDollarsAndCents('0.2'));
+    });
+
+});
--- manager/views/lib/money.js
+++ manager/views/lib/money.js
@@ -6,11 +6,11 @@ exports.toCents = function(dollars) {
     return Math.round(parseFloat(dollars) * 100);
 }
 
-var dollarsAndCentsRegex = /^[1-9][0-9]*(\\.[0-9]{2})?|\\$0?\\.[0-9][0-9]$/;
+// From http://www.stat.berkeley.edu/~nolan/stat133/Fall05/lectures/RegExExamples.html
+var dollarsAndCentsRegex = /^[1-9][0-9]*(\.[0-9]{2})?$|^0?\.[0-9][0-9]$/;
 exports.dollarsAndCentsRegex = dollarsAndCentsRegex;
 
 exports.isDollarsAndCents = function(dollars) {
-    // From http://www.stat.berkeley.edu/~nolan/stat133/Fall05/lectures/RegExExamples.html
-    var matches = dollars.toString.match(dollarsAndCentsRegex);
+    var matches = dollars.toString().match(dollarsAndCentsRegex);
     return (matches && matches.length);
 }

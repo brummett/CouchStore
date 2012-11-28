060d37639821ac321013219965ba3b646f1e27b2
Anthony Brummett <brummett@gmail.com>
2012-11-28 10:11:57 -0600

    remove debugging log()

--- manager/validate_doc_update.js
+++ manager/validate_doc_update.js
@@ -3,7 +3,6 @@ function(newDoc, oldDoc, userCtx) {
     var Validator = require('lib/validate'),
         v;
 
-    log(Validator);
     v = new Validator.Validator(newDoc, oldDoc, userCtx);
     
     v.enforce(userCtx.name, 'You must be logged in to make changes');

3070aedcc3e5e783081d2768300a0197e87c241f
Anthony Brummett <brummett@gmail.com>
2012-11-28 16:22:28 -0600

    Fix bug where the Make a Shipment chooser had bad data
    The bug was introduced when warehouse transfer orders became shippable
    in 6686298f.  Since sale orders have negative quantities, the chooser
    was showing wacky numbers for the number of unshipped items

--- manager/views/shipment-order-picker/map.js
+++ manager/views/shipment-order-picker/map.js
@@ -3,36 +3,21 @@
 // that becomes the "Make a Shipment" drop-down chooser
 function(doc) {
     var order_number = '',
-        barcode,
-        isBackordered = 0,
         shipping = require('views/lib/shipping-priority'),
         Order = require('views/lib/Order'),
         order = Order.newFromDoc(doc),
-        i = 0,
-        count = 0;
+        count;
 
     if (order && order.isShippable()) {
         // order doc IDs start with the string 'order-'
         order_number = order.orderNumber();
-        hasShipments = order.hasShipments();
-        
-        order.barcodes().forEach(function(barcode) {
-            count += order.quantityForBarcode(barcode);  // sale quantities are negative
-        });
 
-        if (hasShipments) {
-            order.shipments().forEach(function(shipment) {
-                var barcode;
-                for (barcode in shipment.items) {
-                    count -= shipment.items[barcode];  // shipment counts are positive
-                }
-            });
-        }
+        count = order.unshippedQuantity();
+
         if (count != 0) {
-            count = Math.abs(count);
             emit([shipping.priority(doc), order_number],
                 { message: order_number + ' ' + count + ' items from '+ doc['warehouse-name'] + ' ' + doc['shipping-service-level'] + ' shipping',
-                  isBackordered: hasShipments
+                  isBackordered: order.hasShipments()
                 });
         }
     }

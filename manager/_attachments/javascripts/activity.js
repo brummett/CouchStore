d655920122b3b32b72d7c07d88f9e17f5ab22a25
Anthony Brummett <brummett@gmail.com>
2012-12-02 19:37:39 -0600

    Refactor to put the jquery.couchapp.js overrides into its own module
    Also added a function for show()

--- manager/_attachments/javascripts/activity.js
+++ manager/_attachments/javascripts/activity.js
@@ -5,67 +5,13 @@ function runActivity(couchapp) {
     // Make a new function for running _update handlers
     // Adapted from jquery.couch.js code
     if (! couchapp.update) {
-        function toJSON(obj) {
-            return obj !== null ? JSON.stringify(obj) : null;
-        }
-        function encodeOptions(options) {
-            // Convert an object into form data
-            var buf = [];
-            if (typeof(options) === "object" && options !== null) {
-                for (var name in options) {
-                    if ($.inArray(name, ["error", "success", "beforeSuccess", "akaxStart"]) >= 0)
-                        continue;
-                    var value = options[name];
-                    if ($.inArray(name, ["key", "startkey", "endkey"]) >= 0) {
-                        value = toJSON(value);
-                    }
-                    buf.push(encodeURIComponent(name) + "=" + encodeURIComponent(value));
-                }
-            }
-            return buf.length ? "?" + buf.join("&") : "";
-        }
-        couchapp.update = function(name, data, options) {
-            var docid = data._id ? data._id : null;
-            var options = options || {};
-            var type = 'POST';
-            var url = '/' + this.db.name + '/' + this.design.doc_id + '/_update/' + name + encodeOptions(options);
-
-            if (docid) {
-                type = 'PUT';
-                url = url + '/' + docid;
-                delete data._id;
-            }
-            delete data._rev;
-
-            $.ajax( $.extend({  type: type,
-                                url: url,
-                                async: true,
-                                dataType: 'json',
-                                data: data,
-                                contentType: 'application/x-www-form-urlencoded',
-                            }, options));
-        };
-        // list() has a bug where the success, error methods don't get passed down to
-        // the ultimate ajax () call
-        couchapp.list = function(list, view, options) {
-            options = options || {};
-            var type = 'GET';
-            var data = null;
-            var url = this.db.uri + '/' + this.design.doc_id + '/_list/' + list + '/' + view + encodeOptions(options);
-            if (options['keys']) {
-                type = 'POST';
-                var keys = options['keys'];
-                delete options['keys'];
-                data = JSON.stringify({ keys: keys });
-            }
-            $.ajax( $.extend({
-                url: url,
-                type: type,
-                async: true,
-                dataType: 'json'
-                }, options));
-        }
-    };
+        (function() {
+            var Couchapp = couchapp.require('lib/Couchapp');
+            couchapp.update = Couchapp.update;
+            couchapp.list = Couchapp.list;
+            couchapp.show = Couchapp.show;
+        })();
+    }
 
     function fade(elt, delay, then) {
         delay = delay === undefined ? 5000 : delay;
@@ -724,9 +670,12 @@ function runActivity(couchapp) {
             var params = context.params.toHash(),
                 docId = context.params['splat'][0] || context.params['_id'] || '',
                 section = context.params.section,
-                next_url = '#/'
+                next_url = '#/';
 
-            couchapp.update('partial-inventory/'+docId, params, {
+            if (docId) {
+                params._id = docId;
+            }
+            couchapp.update('partial-inventory', params, {
                 success: function(orderDoc) {
                     context.showNotification('success', 'Inventory for section ' + orderDoc.section + ' saved!');
                     activity.trigger('inventory-updated', orderDoc);

eddd68a078840bece04441a0aae2454b04509053
Anthony Brummett <brummett@gmail.com>
2012-12-15 16:58:20 -0600

    remove obsolete show functions
    These were used by old, old, old versions of the data lister

--- manager/shows/listItemRow.js
+++
@@ -1,8 +0,0 @@
-function(doc, req) {
-    var ddoc = this;
-    var Mustache = require('vendor/couchapp/lib/mustache');
-
-    if (doc) {
-        return Mustache.to_html(ddoc.templates['listItemRow'], doc);
-    }
-}
